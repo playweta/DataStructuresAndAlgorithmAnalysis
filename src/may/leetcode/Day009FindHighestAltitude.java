439952875413ef37e75b3790392d2293fc1ba230
liubr <1502473931@qq.com>
2022-11-19 11:37:30 +0800

    day009

---
+++ src/may/leetcode/Day009FindHighestAltitude.java
@@ -0,0 +1,21 @@
+package may.leetcode;
+
+/**
+ * @author liubr
+ * @date 2022/11/19 11:27
+ */
+public class Day009FindHighestAltitude {
+	public static void main(String[] args) {
+		System.out.println(new Day009FindHighestAltitude().largestAltitude(new int[]{-5, 1, 5, 0, -7}));
+	}
+
+	public int largestAltitude(int[] gain) {
+		int a = 0;
+		int count = 0;
+		for (int i : gain) {
+			count += i;
+			a = Math.max(a, count);
+		}
+		return a;
+	}
+}
