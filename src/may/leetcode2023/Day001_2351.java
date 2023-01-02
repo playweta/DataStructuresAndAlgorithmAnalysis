0a2d434c5177a7e211944a2a8da6e9f65f6905ee
liubr <1502473931@qq.com>
2023-01-02 16:27:43 +0800

    day001_2351

---
+++ src/may/leetcode2023/Day001_2351.java
@@ -0,0 +1,24 @@
+package may.leetcode2023;
+
+/**
+ * @author liubr
+ * @date 2023/01/01 16:21 下午
+ * 2351. 第一个出现两次的字母
+ */
+public class Day001_2351 {
+	public static void main(String[] args) {
+
+	}
+
+	public char repeatedCharacter(String s) {
+		int[] ints = new int[26];
+		int i = 0;
+		while (true) {
+			char charA = s.charAt(i);
+			if (++ints[charA - 'a'] == 2) {
+				return charA;
+			}
+			++i;
+		}
+	}
+}
