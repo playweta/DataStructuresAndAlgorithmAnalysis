96505a38bf5144680fb33847469157240cf84eed
liubr <1502473931@qq.com>
2023-02-18 17:06:42 +0800

    day006_2341

---
+++ src/may/leetcode2023/Day006_2341.java
@@ -0,0 +1,12 @@
+package may.leetcode2023;
+
+/**
+ * @author liubr
+ * @date 2023/02/16 16:29 下午
+ * 2341. 数组能形成多少数对
+ */
+public class Day006_2341 {
+	public int[] numberOfPairs(int[] nums) {
+        
+	}
+}
